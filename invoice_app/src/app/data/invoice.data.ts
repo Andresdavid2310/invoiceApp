daad238f18be32ea2b00898dbb460834af04170d
Andres Benavides <dandavi2310@hotmail.com>
2024-04-29 14:00:48 -0500

    create services invoice

---
+++ invoice_app/src/app/data/invoice.data.ts
@@ -0,0 +1,38 @@
+export const invoiceData = {
+    id: 1,
+    name: "Componentes de PC",
+    client:{
+        name: 'Andres',
+        lastname: 'Benavides',
+        address: {
+            country: 'COLOMBIA',
+            city: 'Santa Marta',
+            street: 'One Street',
+            number: 15,
+        }
+    },
+    company: {
+        name: 'new age',
+        fiscalNumber: 3123123,
+    },
+    items: [
+        {
+            id:1,
+            product: 'Cpu Intel i9',
+            price: 599,
+            quantity:1,
+        },
+        {
+            id:2,
+            product: 'Mouse Microsoft',
+            price: 200,
+            quantity:1,
+        },
+        {
+            id:3,
+            product: 'Teclado Asus',
+            price: 350,
+            quantity:1,
+        },
+    ]
+}
